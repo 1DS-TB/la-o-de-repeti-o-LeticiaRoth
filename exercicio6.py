343b962448d65c39544abca8e1ddb0c37d97faff
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-04-13 22:14:13 -0300

    Atualização exercício 6

---
+++ exercicio6.py
@@ -0,0 +1,11 @@
+numero = int(input("Digite a quantidade de termos da sequência de Fibonacci: "))
+
+a, b = 0, 1 #Inicia os dois primeiros termos
+
+i = 0
+
+print("Sequência de Fibonacci:")
+while i < numero:
+    print(a, end=" ")
+    a, b = b, a + b
+    i += 1
