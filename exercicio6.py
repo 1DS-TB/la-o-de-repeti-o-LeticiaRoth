af1c9f848b2bda409ebfecf071cd11abffdbecdd
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-04-15 20:02:39 -0300

    Update exercicio6.py

--- exercicio6.py
+++ exercicio6.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-num = int(input("\nDigite um número:"))
-
-if num >= 0: # Verificar se ele é positivo e declarar variáveis
-    soma = 0
-    i = 1
-
-    while i <= num: #Laço para somar os números de 1 até NUM
-        soma += i
-        i += 1
-
-    print(f"A soma de 1 a {num} é: {soma}")
-
-else:
-    print("INVALIDO")
-=======
 numero = int(input("Digite a quantidade de termos da sequência de Fibonacci: "))
 
 a, b = 0, 1 #Inicia os dois primeiros termos
