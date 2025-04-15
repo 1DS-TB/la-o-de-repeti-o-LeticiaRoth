f5d677a99f8dc910e1e0d32765eb3227a89de7d0
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-04-15 20:03:33 -0300

    Atualização

--- exercicio2.py
+++ exercicio2.py
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-num = int(input("Insira um número inteiro positivo: "))
-som = 0
-
-if num < 0:
-    print("INVALIDO")
+num = int(input("\nDigite um número:"))
+if num >= 0: # Verificar se ele é positivo e declarar variáveis
+    soma = 0
+    i = 1
+    while i <= num: #Laço para somar os números de 1 até NUM
+        soma += i
+        i += 1
+    print(f"A soma de 1 a {num} é: {soma}")
 else:
-    num +=1
-    while num > 1:
-        num -= 1
-        som+= num
-print(som)
-=======
-numero = int(input("Digite um número inteiro: "))
-soma = 0
-
-if numero < 0:
     print("INVALIDO")
-else:
-    numero +=1
-    while numero > 1:
-        numero -= 1
-        soma += numero
-print(soma)
->>>>>>> f2747d953f3aa011d969c39c12d3beaa7cecdd31
