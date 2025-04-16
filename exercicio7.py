9f0d536ba0c7a26d38379ca0cf9751364ad03ab5
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-04-16 20:50:28 -0300

    Atualização

--- exercicio7.py
+++ exercicio7.py
@@ -1,6 +1,13 @@
 num = int(input("Digite um número: "))
 
-for ponto in range(1, num +1): #Começa em 1 e vai adicionando mais um até o número
-    linhas = "*" * ponto #Falo que a linha é multiplicado peloa quantidade de ponto
-    print(linhas)
+if num < 1:
+    print("INVALIDO")
+else:
+    for ponto in range(1, num + 1):  #Começa em 1 e vai adicionando mais um até o número
+        linhas = "*" * ponto #Falo que a linha é multiplicado peloa quantidade de ponto
+        print(linhas)
+
+
+
+        
 
