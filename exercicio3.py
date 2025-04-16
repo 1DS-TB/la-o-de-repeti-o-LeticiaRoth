2081314967da2d6f1439954f7d8d7d4c82b36619
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-04-16 16:27:57 -0300

    Atualização exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,6 +1,4 @@
-numero = int(input("Digite um número:"))
+numero = int(input("Digite um número: "))
 
-for num in range (1,11):
-    mul = numero * num
-    print(f"{num} X {numero} = {mul}")
-
+for i in range(1,11):
+    print(numero, "x", i, "=", numero*i)
