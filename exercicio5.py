c0e70b31c6e21692f9da3196f19c31fa469a947a
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-04-16 16:49:49 -0300

    Update exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -1,12 +1,14 @@
 num = int(input("Digite um número: "))
 divisores = 0
 
-for i in range(1, num + 1):
-    if num % i == 0:
-        divisores += 1
-
-if divisores == 2:
-    print(f"{num } é primo")
-
+if num <= 1:
+    print("OK")
 else:
-    print(f"{num} não é primo")
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisores += 1
+
+    if divisores == 2:
+        print("OK")
+    else:
+        print("INVALIDO")
