16fde7289bdabca5a5cfe222b3a00f51ee399e92
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-04-16 16:43:37 -0300

    Correção dos erros

--- exercicio4.py
+++ exercicio4.py
@@ -1,15 +1,10 @@
-numero = int(input("Insira o número que deseja calcular: "))
-i = 1
-nm = 1
+num = int(input("Digite um número:"))
 
-if numero > 0:
-    while i <= numero:
-        nm *= i
-        i+=1
-elif numero < 0:
-    print("INVALIDO")
-    
-elif numero == 0:
-    nm == 1
-print(f"Resultado: {nm}")
-
+if num < 0:
+    print("OK")
+else:
+    fatorial = 1
+    while num > 0:
+        fatorial *= num
+        num -= 1
+    print(f"Resultado: {fatorial}")
