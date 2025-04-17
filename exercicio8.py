55be59d3b1d22a5a398661cf26cd126debecf58d
Letícia Alves Roth <162461539+LeticiaRoth@users.noreply.github.com>
2025-04-16 21:01:45 -0300

    Atualização

--- exercicio8.py
+++ exercicio8.py
@@ -2,7 +2,10 @@ N = int(input("Digite um número: "))
 total = 0
 serie_harmonica = 0
 
-for numeros in range(1, N + 1):
-    total += 1/numeros
-    serie_harmonica = float(total)
-print(f"\nSérie harmônica de {N}: {serie_harmonica:.2f}")
+if N < 1:
+    print("INVALIDO")
+else:
+    for numeros in range(1, N + 1):
+        total += 1/numeros
+        serie_harmonica = float(total)
+    print(f"\nSérie harmônica de {N}: {serie_harmonica:.2f}")
